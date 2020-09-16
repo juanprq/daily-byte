e15dd864879162b73a887cb15361bed46812128c
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-16 16:51:30 -0500

    adds memoization

--- src/knapsack/index.js
+++ src/knapsack/index.js
@@ -1,12 +1,24 @@
 const getValue = (weights, values, capacity) => {
+  const store = [];
+
   const calculate = (weights, values, capacity, index) => {
+    if (store[index] && store[index][capacity]) return store[index][capacity];
     if (index < 0) return 0;
     if (weights[index] > capacity) return calculate(weights, values, capacity, index - 1);
 
     const leftValue = values[index] + calculate(weights, values, capacity - weights[index], index - 1);
     const rightValue = calculate(weights, values, capacity, index - 1);
+    const result = Math.max(leftValue, rightValue);
 
-    return Math.max(leftValue, rightValue);
+    if (store[index]) {
+      store[index][capacity] = result;
+    } else {
+      const aux = [];
+      aux[capacity] = result;
+      store[index] = aux;
+    }
+
+    return result;
   };
 
   return calculate(weights, values, capacity, weights.length - 1);
