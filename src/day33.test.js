3cc8cb4f00842260a907e1a5e629b6e51e565ac2
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-10-10 15:38:04 -0500

    adds more tests for exercise day 33

--- src/day33.test.js
+++ src/day33.test.js
@@ -21,4 +21,36 @@ describe('isEqual', () => {
 
     expect(isEqual(binaryTreeA, binaryTreeB)).toBe(true);
   });
+
+  test('should return false', () => {
+    const nodeA1 = new Node(1);
+    const nodeA2 = nodeA1.addRight(9);
+    nodeA2.addRight(18);
+
+    const nodeB1 = new Node(1);
+    const nodeB2 = nodeB1.addLeft(9);
+    nodeB2.addRight(18);
+
+
+    const binaryTreeA = new BinaryTree(nodeA1);
+    const binaryTreeB = new BinaryTree(nodeB1);
+
+    expect(isEqual(binaryTreeA, binaryTreeB)).toBe(false);
+  });
+
+  test('should return false', () => {
+    const nodeA1 = new Node(2);
+    nodeA1.addLeft(3);
+    nodeA1.addRight(1);
+
+    const nodeB1 = new Node(2);
+    nodeB1.addLeft(1);
+    nodeB1.addRight(3);
+
+
+    const binaryTreeA = new BinaryTree(nodeA1);
+    const binaryTreeB = new BinaryTree(nodeB1);
+
+    expect(isEqual(binaryTreeA, binaryTreeB)).toBe(false);
+  });
 });
