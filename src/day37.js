2c18df6fb2534447b61dd4f4b72f4e723dd3523c
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-10-14 19:23:28 -0500

    adds 37 exercise

--- src/day37.js
+++ src/day37.js
@@ -1,5 +1,22 @@
+const getMax = (node, level, accum = []) => {
+  if (!node) return accum;
+
+  const data = accum[level] || -1;
+
+  if (node.data > data) {
+    accum[level] = node.data;
+  }
+
+  getMax(node.left, level + 1, accum);
+  getMax(node.right, level + 1, accum);
+
+  return accum;
+};
+
 const getMaxPerLevel = (binaryTree) => {
-  return null;
+  const { head } = binaryTree;
+
+  return getMax(head, 0);
 };
 
 module.exports = getMaxPerLevel;
