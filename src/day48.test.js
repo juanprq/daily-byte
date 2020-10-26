8327dd1e3bd25b63bc4c55bf708deb3af05b497a
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-10-25 19:01:03 -0500

    adds test suir for day 48 exercise

---
+++ src/day48.test.js
@@ -0,0 +1,40 @@
+const hasSameLeaves = require('./day48');
+const Node = require('./binaryTree/node');
+
+describe('hasSameLeaves', () => {
+  test('it should be a function', () => {
+    expect(hasSameLeaves).toBeInstanceOf(Function);
+  });
+
+  test('it should return false for different leaves', () => {
+    const head1 = new Node(1);
+    head1.addLeft(1);
+    head1.addRight(3);
+
+    const head2 = new Node(7);
+    head2.addLeft(1);
+    head2.addRight(2);
+
+    expect(hasSameLeaves(head1, head2)).toBe(false);
+  });
+
+  test('it should return true', () => {
+    const headA = new Node(8);
+    headA.addLeft(2);
+    const node29A = headA.addRight(29);
+    node29A.addLeft(3);
+    node29A.addRight(9);
+
+    const headB = new Node(8);
+    headB
+      .addLeft(2)
+      .addLeft(2);
+
+    const node29B = headB.addLeft(3);
+    const node3B = node29B.addLeft(3);
+    node3B.addRight(3);
+    node29B.addRight(9);
+
+    expect(hasSameLeaves(headA, headB)).toBe(true);
+  });
+});
