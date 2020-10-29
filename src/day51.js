369a2084fb1b89b9fa453476b59331c33e1e6ac4
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-10-29 14:25:02 -0500

    adds solution for day 51

--- src/day51.js
+++ src/day51.js
@@ -9,33 +9,34 @@ const digitMapping = {
   '7': 'pqrs',
   '8': 'tuv',
   '9': 'wxyz',
-}
+};
+
+const combine = (space) => {
+  if (space.length === 0) return [];
+  if (space.length === 1) return space[0];
+  if (space.length === 2) {
+    const [head, tail] = space;
+    return head.reduce((accum, headCharacter) => {
+      const words = tail.map(tailCharacter => headCharacter + tailCharacter);
+      return [...accum, ...words];
+    }, []);
+  }
+
+  const [head, ...rest] = space;
+  return test([head, test(rest)]);
+};
 
 const getStringOfDigits = (digitsStr) => {
   const digits = digitsStr.split('');
-  const mapping = digits.reduce((accum, digit) => {
+  const space = digits.reduce((accum, digit) => {
     const map = digitMapping[digit];
     if (!map) return accum;
 
     return [...accum, map.split('')];
   }, []);
 
-
-  if (mapping.length === 0) return mapping;
-  if (mapping.length === 1) return mapping[0];
-
-  const result = [];
-  for (let i = 0; i < mapping[0].length; i++) {
-    const firstChar = mapping[0][i];
-
-    for (let j = 0; j < mapping[1].length; j++) {
-      const secondChar = mapping[1][j];
-
-      result.push(firstChar + secondChar);
-    }
-  }
-
-  return result;
+  return combine(space);
 };
+
 
 module.exports = getStringOfDigits;
--- src/day51.test.js
+++ src/day51.test.js
@@ -5,44 +5,26 @@ describe('getStringOfDigits', () => {
     expect(getStringOfDigits).toBeInstanceOf(Function);
   });
 
-  // test('should return for null mapping', () => {
-  //   expect(getStringOfDigits('0')).toEqual([]);
-  // });
-
-  // test('should return for 1 digit', () => {
-  //   expect(getStringOfDigits('9')).toEqual(['w', 'x', 'y', 'z']);
-  // });
-
-  // test('should return all combinations', () => {
-  //   expect(getStringOfDigits('23'))
-  //     .toEqual([
-  //       'ad',
-  //       'ae',
-  //       'af',
-  //       'bd',
-  //       'be',
-  //       'bf',
-  //       'cd',
-  //       'ce',
-  //       'cf',
-  //     ]);
-  // });
-
-  test('test 0 elements', () => {
-    expect(
-      getStringOfDigits([])
-    ).toEqual([]);
+  test('should return for null mapping', () => {
+    expect(getStringOfDigits('0')).toEqual([]);
   });
 
-  test('test 1 element', () => {
-    expect(
-      getStringOfDigits([['a', 'b']])
-    ).toEqual(['a', 'b']);
+  test('should return for 1 digit', () => {
+    expect(getStringOfDigits('9')).toEqual(['w', 'x', 'y', 'z']);
   });
 
-  test('test 2 elements', () => {
-    expect(
-      getStringOfDigits([['a', 'b'], ['c', 'd']])
-    ).toEqual(['ac', 'ad', 'bc', 'bd']);
+  test('should return all combinations', () => {
+    expect(getStringOfDigits('23'))
+      .toEqual([
+        'ad',
+        'ae',
+        'af',
+        'bd',
+        'be',
+        'bf',
+        'cd',
+        'ce',
+        'cf',
+      ]);
   });
 });
