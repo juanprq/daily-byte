d4e2498f8b9ad5d7235afe712752d7b1b50bd26d
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-10-29 12:50:46 -0500

    adds day 51 test suit

--- src/day51.test.js
+++ src/day51.test.js
@@ -5,26 +5,44 @@ describe('getStringOfDigits', () => {
     expect(getStringOfDigits).toBeInstanceOf(Function);
   });
 
-  test('should return for null mapping', () => {
-    expect(getStringOfDigits('0')).toEqual([]);
-  });
+  // test('should return for null mapping', () => {
+  //   expect(getStringOfDigits('0')).toEqual([]);
+  // });
 
   // test('should return for 1 digit', () => {
   //   expect(getStringOfDigits('9')).toEqual(['w', 'x', 'y', 'z']);
   // });
 
-  test('should return all combinations', () => {
-    expect(getStringOfDigits('23'))
-      .toEqual([
-        'ad',
-        'ae',
-        'af',
-        'bd',
-        'be',
-        'bf',
-        'cd',
-        'ce',
-        'cf',
-      ]);
+  // test('should return all combinations', () => {
+  //   expect(getStringOfDigits('23'))
+  //     .toEqual([
+  //       'ad',
+  //       'ae',
+  //       'af',
+  //       'bd',
+  //       'be',
+  //       'bf',
+  //       'cd',
+  //       'ce',
+  //       'cf',
+  //     ]);
+  // });
+
+  test('test 0 elements', () => {
+    expect(
+      getStringOfDigits([])
+    ).toEqual([]);
+  });
+
+  test('test 1 element', () => {
+    expect(
+      getStringOfDigits([['a', 'b']])
+    ).toEqual(['a', 'b']);
+  });
+
+  test('test 2 elements', () => {
+    expect(
+      getStringOfDigits([['a', 'b'], ['c', 'd']])
+    ).toEqual(['ac', 'ad', 'bc', 'bd']);
   });
 });
