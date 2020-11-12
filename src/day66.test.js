721ff7f085f40066feca6282d63606b772ef17c7
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-11-12 18:25:27 -0500

    adds more test to day 66

--- src/day66.test.js
+++ src/day66.test.js
@@ -5,10 +5,33 @@ describe('makeChange', () => {
     expect(makeChange).toBeInstanceOf(Function);
   });
 
-  test('it should return 6', () => {
+  // test('it should return 6', () => {
+  //   const coins = [1, 5, 10, 25];
+  //   const amount = 78;
+
+  //   expect(makeChange(coins, amount)).toEqual(6);
+  // });
+
+  test('it should return 1', () => {
+    const coins = [1, 5];
+    const amount = 5;
+
+    expect(makeChange(coins, amount)).toEqual(1);
+  });
+
+  test('it should return 2', () => {
+    const coins = [1, 2, 3];
+    const amount = 6;
+
+    expect(makeChange(coins, amount)).toEqual(2);
+  });
+
+
+  test('it should return 1', () => {
     const coins = [1, 5, 10, 25];
-    const amount = 78;
+    const amount = 54;
 
     expect(makeChange(coins, amount)).toEqual(6);
   });
+
 });
