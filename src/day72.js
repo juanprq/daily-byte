0f1e89aed862c7c4b71b3029f68ac1dadcf06894
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-11-18 19:58:30 -0500

    all possibilities approach

--- src/day72.js
+++ src/day72.js
@@ -1,13 +1,19 @@
-const getMinimumOperations = (s, t) => {
-  let operations = Math.abs(s.length - t.length);
-
-  for(let i = 0; i < s.length; i++) {
-    if (s.charAt(i) !== t.charAt(i)) {
-      operations++;
-    }
+const getOperations = (s, t, n, m) => {
+  if (n == 0) return m; // insert all letters
+  if (m == 0) return n; // delete all letters
+  if (s.charAt(n - 1) === t.charAt(m - 1)) {
+    return getOperations(s, t, n - 1, m -1);
   }
 
-  return operations;
+  return 1 + Math.min(
+    getOperations(s, t, n, m - 1), // insert
+    getOperations(s, t, n - 1, m - 1), // delete
+    getOperations(s, t, n - 1, m - 1), // replace
+  );
+};
+
+const getMinimumOperations = (s, t) => {
+  return getOperations(s, t, s.length, t.length);
 };
 
 module.exports = getMinimumOperations;
