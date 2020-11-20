3fbec421409a50d9172f97199bda1f89fa738fe4
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-11-19 22:58:50 -0500

    adds another insertion sort implementation

--- src/insertionSort/index.js
+++ src/insertionSort/index.js
@@ -2,13 +2,19 @@ const sort = (array) => {
   const copy = [...array];
 
   for (let i = 1; i < copy.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (copy[i] < copy[j]) {
-        const aux = copy[j];
-        copy[j] = copy[i];
-        copy[i] = aux;
-      }
+    const pivot = copy[i];
+
+    let j = i - 1;
+    while (j >= 0 && pivot < copy[j]) {
+      // move array to right
+      copy[j + 1] = copy[j];
+      j--;
     }
+
+    // when we found the correct positio of pivor
+    // that is left < pivor < right
+    // insert it
+    copy[j + 1] = pivot;
   }
 
   return copy;
