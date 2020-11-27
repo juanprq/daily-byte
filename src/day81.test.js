59ec55eb3265392b8fd3868bc58ad4216e2a47aa
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-11-27 12:54:57 -0500

    adds day 81 test suit

---
+++ src/day81.test.js
@@ -0,0 +1,11 @@
+const flipDigits = require('./day81');
+
+describe('flipDigits', () => {
+  test('it should be a function', () => {
+    test(flipDigits).toBeInstanceOf(Function);
+  });
+
+  test('it should return 4', () => {
+    expect(flipDigits(27)).toEqual(4);
+  });
+});
