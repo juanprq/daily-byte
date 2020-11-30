f071b3c901c25b0e6d9746e99dc835cbcc45eaab
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-11-30 14:50:13 -0500

    update test suit for day 83

--- src/day83.test.js
+++ src/day83.test.js
@@ -5,9 +5,9 @@ describe('getPrimseUntilN', () => {
     expect(getPrimesUntilN).toBeInstanceOf(Function);
   });
 
-  // test('it should return 1', () => {
-  //   expect(getPrimesUntilN(3)).toEqual(1);
-  // });
+  test('it should return 1', () => {
+    expect(getPrimesUntilN(3)).toEqual(1);
+  });
 
   test('it should return 3', () => {
     expect(getPrimesUntilN(7)).toEqual(3);
