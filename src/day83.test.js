04f7afe25877a59e78c4c763f8c75387770bcf69
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-11-30 14:48:49 -0500

    adds day 83 test suit

---
+++ src/day83.test.js
@@ -0,0 +1,15 @@
+const getPrimesUntilN = require('./day83');
+
+describe('getPrimseUntilN', () => {
+  test('it should be a function', () => {
+    expect(getPrimesUntilN).toBeInstanceOf(Function);
+  });
+
+  // test('it should return 1', () => {
+  //   expect(getPrimesUntilN(3)).toEqual(1);
+  // });
+
+  test('it should return 3', () => {
+    expect(getPrimesUntilN(7)).toEqual(3);
+  });
+});
