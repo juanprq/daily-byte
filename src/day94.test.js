8c8f1292c7d4588170115d3f3a6f856fc215ddd8
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-12-14 21:07:51 -0500

    adds day 94 test suit

---
+++ src/day94.test.js
@@ -0,0 +1,17 @@
+const minimizePath = require('../day94');
+
+describe('minimizePath', () => {
+  test('it should be a function', () => {
+    expect(minimizePath).toBeInstanceOf(Function);
+  });
+
+  test('it should return 7', () => {
+    const grid = [
+      [1, 1, 3],
+      [2, 3, 1],
+      [4, 6, 1],
+    ];
+
+    expect(minimizePath(grid)).toEqual(7);
+  });
+});
